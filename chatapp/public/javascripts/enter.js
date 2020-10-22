e8064ffd7637da964bdc9d27a7f41ba4de612e8d
tanayane <tanaka.ayane125@gmail.com>
2020-10-22 19:01:42 +0900

    入室メッセージの送信

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -2,11 +2,12 @@
 
 // 入室メッセージをサーバに送信する
 // 入力されたユーザ名を取得する
-const userName = '';
+const userName = $('#userName').val();
 // 入室メッセージイベントを送信する
+socket.emit('enterEvent', userName + 'さんが入室しました');
 
 
 // サーバから受信した入室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('sendEnterMessage', function (data) {
+    $('#thread').prepend('<p>' + data + '</p>');
 });
--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,7 +2,8 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-    socket.on('', function (data) {
-
+    socket.on('enterEvent', function (data) {
+        //自分以外に送信
+        socket.broadcast.emit('sendEnterMessage', data);
     });
 };
